85ca61791b20ac5ec225f03e64a6e162f18b96eb
kkxop88079 <shuwei@ceis.tw>
2022-09-28 13:27:51 +0800

    [Feature] 新增文字

--- src/components/work/index.vue
+++ src/components/work/index.vue
@@ -1,9 +1,29 @@
 <template>
-    <div class="w-screen h-screen flex">
-        <div class="w-[1360px]">
-            <div class="border p-4 flex" data-aos="fade-down">
-                <div class="flex-1">123</div>
-                <div class="flex-1">456</div>
+    <div class="w-screen h-screen flex justify-center">
+        <div class="w-[1360px] pt-32">
+            <div class="border p-4 flex mb-10" data-aos="fade-up">
+                <div class="flex-1">
+                    <div class="py-20 px-6">
+                        <p class="text-3xl mb-6 font-bold">擴大您的觸及範圍</p>
+                        <p class="text-lg mb-10">定義塑造我們文化的流行趨勢，並將你的 AR 特效與全球數億使用者分享。</p>
+                        <a href="" class="p-2 border rounded-md px-4 border-[#7709ee] text-[#7709ee]">開始創作</a>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <img src="../../assets/helios1.png" alt="">
+                </div>
+            </div>
+            <div class="border p-4 flex mt-10" data-aos="fade-up" >
+                <div class="flex-1">
+                    <div class="py-20 px-6">
+                        <p class="text-3xl mb-6 font-bold">擴大您的觸及範圍</p>
+                        <p class="text-lg mb-10">定義塑造我們文化的流行趨勢，並將你的 AR 特效與全球數億使用者分享。</p>
+                        <a href="" class="p-2 border rounded-md px-4 border-[#7709ee] text-[#7709ee]">開始創作</a>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <img src="../../assets/helios1.png" alt="">
+                </div>
             </div>
         </div>
     </div>
